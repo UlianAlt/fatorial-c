ddf8dcb95dab79b87309fc818675a238caf66697
UlianAlt <zpedro237732@gmail.com>
2026-05-13 20:34:48 -0300

    Change void main to int main in test file

--- teste_fatorial.c
+++ teste_fatorial.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <assert.h>
 
-void main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
 	assert(fatorar(0) == 1 && fatorar(1) == 1 && fatorar(6) == 720);
 	printf("Teste concluido com sucesso");
+	return 0;
 }
