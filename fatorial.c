193afabe2b82a3fd1460391546828ef2f85f54f6
UlianAlt <zpedro237732@gmail.com>
2026-07-29 19:44:12 -0300

    Modify main function to log results to fatorial.txt
    Added file output for factorial results and messages.

--- fatorial.c
+++ fatorial.c
@@ -2,12 +2,20 @@
 #include "function_fatorial.c"
 #include <stdlib.h>
 
-void main(int argc, const char *argv[]){
+int main(int argc, const char *argv[]){
+	FILE *arquivo = fopen("fatorial.txt", "w");
+
 	if(argc != 2){
         printf("digite o nome do programa seguido de 1 argumento (Fatorial 5)");
+		fprintf(arquivo, "digite o nome do programa seguido de 1 argumento (Fatorial 5)");
     }else{
     	int input = atoi(argv[1]);
 		printf("resultado: %d\n", fatorar(input));
+		fprintf(arquivo, "resultado: %d\n", fatorar(input));
 		printf("\nFeito por PedroUlian (https://github.com/PedroUlian) (https://github.com/UlianAlt)");
+		fprintf(arquivo, "\nFeito por PedroUlian (https://github.com/PedroUlian) (https://github.com/UlianAlt)");
     }
+
+	fclose(arquivo);
+	return 0;
 }
